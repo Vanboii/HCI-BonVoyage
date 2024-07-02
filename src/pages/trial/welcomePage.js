aa475efa8c1180a26235447b79837b2d1be93e03
michellehalim <m.chellehlm@gmail.com>
2024-07-02 22:33:24 +0800

    fix: Folder of the pages

---
+++ src/pages/trial/welcomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TopBanner from "../../components/banner";
+import ButtonComponent from "../../components/button";
+import "./homePage.css";
+
+
+
+function WelcomePage() {
+
+    return (
+        <>
+        <TopBanner/>
+        <div className="main">
+            Hello
+        </div>
+        </>
+
+        
+
+    )
+
+}
+
+export default WelcomePage;
