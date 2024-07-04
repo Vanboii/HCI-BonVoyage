8d934f82851beb67f2dbe32495106e19c6970c29
michellehalim <m.chellehlm@gmail.com>
2024-07-04 21:10:50 +0800

    fix: routes fix pt 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Default from './pages/default';
 import HomePage from './pages/home_page/homePage';
-import LoginPage from './pages/login/signup_page/loginPage';
-import SignupPage from './pages/login/signup_page/signupPage';
+import LoginPage from './pages/login_page/loginPage';
+import SignupPage from './pages/signup_page/signupPage';
 import WelcomePage from './pages/trial/welcomePage';
 import CommunityPage from './pages/community_page/communityPage';
+import MyTripsPage from './pages/mytrips_page/mytripsPage';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/welcome' element={<WelcomePage />} />
-        <Route path='/newpage' element={<CommunityPage />} />
+        <Route path='/community' element={<CommunityPage />} />
+        <Route path='/mytrips' element={<MyTripsPage />} />
         <Route path='*' element={<HomePage />} />
       </Routes>
     </Router>
--- src/pages/community_page/communityPage.js
+++ src/pages/community_page/communityPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import TopBanner from "../components/banner";
-import ButtonComponent from "../components/button";
+import TopBanner from "../../components/banner";
+import ButtonComponent from "../../components/button";
 import "../home_page/homePage.css";
 
 
