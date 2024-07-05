43235750d1f3ba661aac691d6b2a00e81eee4fc2
Sofeanna17 <1007016@mymail.sutd.edu.sg>
2024-07-05 22:46:44 +0800

    feature: preference page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import LoginPage from './pages/login/signup_page/loginPage';
 import SignupPage from './pages/login/signup_page/signupPage';
 import WelcomePage from './pages/trial/welcomePage';
 import CommunityPage from './pages/community_page/communityPage';
-import TripDetailPage from './pages/planning_page/trip_detail/trip_detail';  // Adjusted import to match the file path and name
+import TripDetailPage from './pages/planning_page/trip_detail/trip_detail'; // Adjusted import to match the file path and name
 import InvitePage from './pages/planning_page/invite_page/invite_page'; // Corrected import path
+import PreferencesPage from './pages/planning_page/preferences_page/preferences_page'; // Import PreferencesPage
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
                 <Route path='/newpage' element={<CommunityPage />} />
                 <Route path='/planning/trip_detail' element={<TripDetailPage />} />
                 <Route path='/planning/invite' element={<InvitePage />} /> {/* Corrected path */}
+                <Route path='/preferences' element={<PreferencesPage />} /> {/* Added PreferencesPage route */}
                 <Route path='*' element={<HomePage />} />
             </Routes>
         </Router>
