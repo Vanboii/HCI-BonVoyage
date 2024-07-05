a72e8ff7452d5560dd4d3b1b82915436c207fa02
Sofeanna17 <1007016@mymail.sutd.edu.sg>
2024-07-05 23:20:22 +0800

    feature: pref page

--- src/pages/planning_page/preferences_page/preferences_page.js
+++ src/pages/planning_page/preferences_page/preferences_page.js
@@ -7,7 +7,7 @@ const PreferencesPage = () => {
     <div className="preferences-container">
       <TopBanner />
       <main>
-        <h1>Preferences</h1>
+        <h1>Personal Preference</h1>
         <p>Customise your Travel Experience!</p>
         <form>
           <div className="form-group">
@@ -31,19 +31,25 @@ const PreferencesPage = () => {
           <div className="form-group">
             <label>Category of Activities:</label>
             <div className="options">
-              <button type="button">Museum</button>
+              <button type="button">Museums</button>
               <button type="button">Shopping</button>
               <button type="button">Amusement Park</button>
               <button type="button">Historical Site</button>
+              <button type="button">Kid-Friendly</button>
+              <button type="button">Pet-Friendly</button>
+              <button type="button">Wheelchair Friendly</button>
+              <button type="button">Parks & Scenic Plane</button>
+              <button type="button">Theater & Cultural </button>
+              <button type="button">Food Galore</button>
             </div>
           </div>
           <div className="form-group">
             <label>Estimated Budget:</label>
             <div className="options">
-              <button type="button">(SGD) 500 - 750</button>
-              <button type="button">(SGD) 751 - 1,000</button>
-              <button type="button">(SGD) 1,001 - 1,250</button>
-              <button type="button">(SGD) 1,251++</button>
+              <button type="button">$500 - $750</button>
+              <button type="button">$751 - $1,000</button>
+              <button type="button">$1,001 - $1,250</button>
+              <button type="button">$1,251++</button>
             </div>
           </div>
           <button type="button" className="next-button">Next</button>
