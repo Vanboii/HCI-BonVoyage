4fe7852f1235c83c14f8bc4cfdd724c640a4fe1d
wsnc0 <celest_ng@mymail.sutd.edu.sg>
2024-07-13 17:28:42 +0800

    feat. inproving UI of frontpage

--- src/pages/home_page/homePage.js
+++ src/pages/home_page/homePage.js
@@ -1,13 +1,40 @@
-import React from "react";
-import TopBanner from '../../components/banner'; 
-import ButtonComponent from '../../components/button'; 
-import './homePage.css'; 
+import React, { useEffect, useState } from "react";
+import HomePageBanner from '../../components/homepagebanner';
+import ButtonComponent from '../../components/button';
+import './homePage.css';
+
+const images = [
+    require('../../components/homepage_slideshow/cebu_slideshow.jpg'),
+    require('../../components/homepage_slideshow/singapore_slideshow.jpg'),
+    require('../../components/homepage_slideshow/hawaii_slideshow.jpg'),
+    require('../../components/homepage_slideshow/jeju_slideshow.jpg'),
+    require('../../components/homepage_slideshow/maldives_slideshow.jpg'),
+    require('../../components/homepage_slideshow/mountain_slideshow.jpg'),
+    require('../../components/homepage_slideshow/switzerland_slideshow.jpg'),
+    require('../../components/homepage_slideshow/newzealand_slideshow.jpg'),
+    require('../../components/homepage_slideshow/norway_slideshow.jpg')
+];
 
 function HomePage() {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [fade, setFade] = useState(true); // Initial fade state set to true
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFade(false); // Start fading out the current image
+            setTimeout(() => {
+                setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+                setFade(true); // Start fading in the next image after a delay
+            }, 300); // Delay before fading in the next image (match your CSS transition duration)
+        }, 6000); // Interval between image changes
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, []);
+
     return (
         <>
-            <TopBanner />
-            <div id="main">
+            <HomePageBanner />
+            <div id="main" className={fade ? 'fade' : ''} style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
                 <div className="col centerAlign">
                     <h1>Bon Voyage</h1>
                     <ButtonComponent type="2" text="Start Planning" toPage="/planning/trip_detail" />
@@ -18,3 +45,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
