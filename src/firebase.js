6dd8fb1dffdaf8898f2d9fa9da3c44356cbdc4b0
Vanboii <144983255+Vanboii@users.noreply.github.com>
2024-08-03 01:44:33 +0800

    firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,27 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
-import { Firestore, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB7saEhVsScBEWEmP0LDIQy0Rc7qZQ4a7s",
-  authDomain: "hci-bonvoyage.firebaseapp.com",
-  projectId: "hci-bonvoyage",
-  storageBucket: "hci-bonvoyage.appspot.com",
-  messagingSenderId: "877723332161",
-  appId: "1:877723332161:web:a4fa09e2f23f5589a1a0c0",
-  measurementId: "G-Z19X17D93J"
+  apiKey: "AIzaSyCWfzHRAMccFuUYM7GhPssyBjml-b4csBA",
+  authDomain: "bonvoyage-ab-testing.firebaseapp.com",
+  projectId: "bonvoyage-ab-testing",
+  storageBucket: "bonvoyage-ab-testing.appspot.com",
+  messagingSenderId: "1014608347865",
+  appId: "1:1014608347865:web:f191eec1d4ad8d40b974c8",
+  measurementId: "G-624SK77LR3"
 };
 
 // Initialize Firebase
-const firebase_app = initializeApp(firebaseConfig);
-export const auth = getAuth(firebase_app);
-export const db = getFirestore(firebase_app);
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-
-
-
-//# hci-bonvoyage.web.app
+//#  https://bonvoyage-ab-testing.web.app/
